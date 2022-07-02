31de2723d97d1918a64b224ff0c841432a90d5a0
Divyansh Singh <kalki@Divyanshs-MacBook-Air.local>
2022-07-02 22:49:51 +0530

    Solve eslint

--- script.js
+++ script.js
@@ -3,74 +3,74 @@ const hamburger = document.querySelector('.hamburger');
 const hamlist = document.querySelector('.ham-list');
 
 hamburger.addEventListener('click', () => {
-    hamburger.classList.toggle("active");
-    hamlist.classList.toggle("active");     
+  hamburger.classList.toggle('active');
+  hamlist.classList.toggle('active');
 });
 
 hamlist.addEventListener('click', () => {
-    hamburger.classList.remove("active");
-    hamlist.classList.remove("active");
-})
+  hamburger.classList.remove('active');
+  hamlist.classList.remove('active');
+});
 
 const animelist = [
-    {
-        serial: "first",
-        imageurl: "images/one-piece.jpeg",
-        animename: "One Piece",
-        animesub: "Adventure",
-        animedes: "One Piece is an anime series"
-    },
+  {
+    serial: 'first',
+    imageurl: 'images/one-piece.jpeg',
+    animename: 'One Piece',
+    animesub: 'Adventure',
+    animedes: 'One Piece is an anime series',
+  },
 
-    {
-        serial: "second",
-        imageurl: "images/narutoanime.png",
-        animename: "Naruto",
-        animesub: "Action",
-        animedes: "Naruto is a popular anime series"
-    },
+  {
+    serial: 'second',
+    imageurl: 'images/narutoanime.png',
+    animename: 'Naruto',
+    animesub: 'Action',
+    animedes: 'Naruto is a popular anime series',
+  },
 
-    {
-        serial: "third",
-        imageurl: "images/clannad.jpg",
-        animename: "Clannad",
-        animesub: "Romance",
-        animedes: "Clannad is a very sweet romance anime"
-    },
-    
-    {
-        serial: "fourth",
-        imageurl: "images/Deathnote.png",
-        animename: "Death Note",
-        animesub: "Crime",
-        animedes: "Death Note is a popular thriller anime"
-    },
+  {
+    serial: 'third',
+    imageurl: 'images/clannad.jpg',
+    animename: 'Clannad',
+    animesub: 'Romance',
+    animedes: 'Clannad is a very sweet romance anime',
+  },
 
-    {
-        serial: "fifth",
-        imageurl: "images/Code.jpg",
-        animename: "Code Geass",
-        animesub: "Thriller",
-        animedes: "Code Geass is an exciting thriller anime"
-    },
+  {
+    serial: 'fourth',
+    imageurl: 'images/Deathnote.png',
+    animename: 'Death Note',
+    animesub: 'Crime',
+    animedes: 'Death Note is a popular thriller anime',
+  },
 
-    {
-        serial: "sixth",
-        imageurl: "images/Pancreas.jpg",
-        animename: "I want to eat your Pancreas",
-        animesub: "Romance",
-        animedes: "I want to eat your pancreas<br> is a heart touching anime"
-    }
+  {
+    serial: 'fifth',
+    imageurl: 'images/Code.jpg',
+    animename: 'Code Geass',
+    animesub: 'Thriller',
+    animedes: 'Code Geass is an exciting thriller anime',
+  },
 
-]
+  {
+    serial: 'sixth',
+    imageurl: 'images/Pancreas.jpg',
+    animename: 'I want to eat your Pancreas',
+    animesub: 'Romance',
+    animedes: 'I want to eat your pancreas<br> is a heart touching anime',
+  },
 
-const ulli = document.getElementById("anime-list");
-const deskli = document.getElementById("desk-anime-list");
+];
+
+const ulli = document.getElementById('anime-list');
+const deskli = document.getElementById('desk-anime-list');
 
 const additems = () => {
- let card = "";
- 
- for(let i = 0; i <animelist.length; i++){
-    let items = `
+  let card = '';
+
+  for (let i = 0; i < animelist.length; i += 1) {
+    const items = `
     <ul class="${animelist[i].serial}">
     <li><img class="an1" src="${animelist[i].imageurl}" alt="anime"></li>
     <div class="li-items>
@@ -84,27 +84,26 @@ const additems = () => {
     </div>
     </ul>
     <br>
-    `  
+    `;
     card += items;
- }
- ulli.innerHTML = card;
+  }
+  ulli.innerHTML = card;
 
- deskli.innerHTML = card;
-}
+  deskli.innerHTML = card;
+};
 
-additems ();
+additems();
 
-const press = document.querySelector(".more");
-const hide = document.querySelector(".third");
-const hide2 = document.querySelector(".fourth");
-const hide3 = document.querySelector(".fifth");
-const hide4 = document.querySelector(".sixth");
+const press = document.querySelector('.more');
+const hide = document.querySelector('.third');
+const hide2 = document.querySelector('.fourth');
+const hide3 = document.querySelector('.fifth');
+const hide4 = document.querySelector('.sixth');
 
 press.addEventListener('click', () => {
-  press.classList.toggle("active");
-  hide.classList.toggle("active");
-  hide2.classList.toggle("active");
-  hide3.classList.toggle("active");
-  hide4.classList.toggle("active");
-  press2.classList.toggle("active");
+  press.classList.toggle('active');
+  hide.classList.toggle('active');
+  hide2.classList.toggle('active');
+  hide3.classList.toggle('active');
+  hide4.classList.toggle('active');
 });
